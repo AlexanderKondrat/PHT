a85dc88927dadaa36b80d22d063dac5ac815eaab
AlexanderKondrat <47692437+AlexanderKondrat@users.noreply.github.com>
2019-02-19 23:34:14 +0300

    Task1 (#1)

---
+++ Занятие 1/Task2.java
@@ -0,0 +1,11 @@
+package library;
+
+public class Task2 {
+	public static void main(String[] args) {
+		int a=1;
+		double b=2.0;
+		char c='x';
+		//float=2.2;
+		System.out.println(a + "+" + b +"=" +c);
+}
+}
---
+++ Занятие 1/Task3.java
@@ -0,0 +1,17 @@
+package library;
+
+public class Task3 {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=5;
+if (a>b)		
+	System.out.println(a);
+else if (a>c) 
+	c++;
+	System.out.println(c);
+	
+	
+	
+	}
+}
